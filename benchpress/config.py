1031d730d3e24178b7020eda5976c4dd78025220
Paul Nation <nonhermitian@gmail.com>
2024-07-21 10:41:52 -0400

    fix gym name

--- benchpress/config.py
+++ benchpress/config.py
@@ -80,7 +80,7 @@ class BenchpressConfig:
         if self.gym_name is None:
             raise ValueError("gym_name not set")
 
-        if self.gym_name in ["qiskit", "tket", "bqskit"]:
+        if self.gym_name in ["qiskit", "tket", "bqskit", "qiskit-transpiler-service"]:
             backend = get_backend(
                 backend_name=self.options["general"]["backend_name"],
                 gym_name=self.gym_name,
