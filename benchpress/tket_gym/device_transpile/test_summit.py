52ee9b7c9e0373aaf749bebbbef038d488d0eaa8
ShellyGarion <shelly@il.ibm.com>
2024-07-25 08:25:58 -0500

    add test_clifford_100_transpile to tket_gym

--- benchpress/tket_gym/device_transpile/test_summit.py
+++ benchpress/tket_gym/device_transpile/test_summit.py
@@ -143,3 +143,20 @@ class TestWorkoutDeviceTranspile100Q(WorkoutDeviceTranspile100Q):
 
         output_circuit_properties(result, TWO_Q_GATE, benchmark)
         assert circuit_validator(result, BACKEND)
+
+    def test_clifford_100_transpile(self, benchmark):
+        """Compile 10Q Clifford circuit against target backend"""
+        circuit = qasm_circuit_loader(
+            Configuration.get_qasm_dir("clifford") + "clifford_100_12345.qasm", benchmark
+        )
+        pm = BACKEND.default_compilation_pass(optimisation_level=OPTIMIZATION_LEVEL)
+
+        @benchmark
+        def result():
+            # Need to make a copy as the compilation is done in-place
+            new_circ = circuit.copy()
+            pm.apply(new_circ)
+            return new_circ
+
+        output_circuit_properties(result, TWO_Q_GATE, benchmark)
+        assert circuit_validator(result, BACKEND)
